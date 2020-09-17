beb84ee4da2a8e23912644a3ce9c10a9a21bca82
muhammadsalmancoder <shanshaikh891@gmail.com>
2020-09-17 22:11:36 +0500

    shan shaikh Chat App

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,29 @@
+﻿// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCE9-WwL_mJiKMBZ8svOcHmZ4eIavIlwv8",
+    authDomain: "chat-app-89ca4.firebaseapp.com",
+    databaseURL: "https://chat-app-89ca4.firebaseio.com",
+    projectId: "chat-app-89ca4",
+    storageBucket: "chat-app-89ca4.appspot.com",
+    messagingSenderId: "670159223241",
+    appId: "1:670159223241:web:a15a41b670c77113559018",
+    measurementId: "G-D51S0CV0X6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+// // Your web app's Firebase configuration
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBxhkOKc_tWaeeUSePIwitc2Tm-aqrLTww",
+//     authDomain: "chat-app-7da13.firebaseapp.com",
+//     databaseURL: "https://chat-app-7da13.firebaseio.com",
+//     projectId: "chat-app-7da13",
+//     storageBucket: "",
+//     messagingSenderId: "454878238535",
+//     appId: "1:454878238535:web:6fb06475d86f38e4"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
